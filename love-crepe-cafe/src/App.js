b8e381bbdb929e0e0c2d31fc18936a18e26957f2
GUPGHTH <pptoncom@gmail.com>
2024-03-09 00:20:36 +0700

    backend ver1

--- love-crepe-cafe/src/App.js
+++ love-crepe-cafe/src/App.js
@@ -3,15 +3,19 @@ import Layout from './page/Layout.js';
 import HomeCustomer from './page/Home_customer.jsx';
 import NoPage from './page/NoPage.jsx';
 import Listmenu from './page/ListMenu.jsx';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter,
+  Switch,
+  Route,
+  Routes,
+  Redirect, } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomeCustomer />} />
-          <Route path='list' element={<Listmenu />} />
+          <Route path='/:lang' element={<HomeCustomer />}></Route>
+          <Route path='list/:lang' element={<Listmenu />}></Route>
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
--- love-crepe-cafe/src/page/ListMenu.jsx
+++ love-crepe-cafe/src/page/ListMenu.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import Style from "../Listmenu/Listmenu.module.css";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 import Addanddel from "../components/addanddel/addanddel";
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+
 const menulist = [{ num: 1, name: "Padthai", price: 200 }, { num: 2, name: "Cake", price: 300 }, { num: 3, name: "Coke", price: 100 }];
 
 
@@ -10,11 +14,30 @@ const menulist = [{ num: 1, name: "Padthai", price: 200 }, { num: 2, name: "Cake
 
 export default function Listmenu() {
 
-    const [menulist_us, setmenulist_us] = useState(menulist);
+    const { lang } = useParams();
+
+    const [menulist_us, setmenulist_us] = useState([]);
+
+    useEffect(() => {
+        getmenulist(lang);
+    }, [])
+
+    const getmenulist = async (lang) => {
+        try {
+            const res = await axios.post('http://localhost:5177/getmenu', {
+                lang: lang
+            });
+            setmenulist_us(res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     // setmenulist_us(menulist);
+
     return (
         <Container className="my-4">
-            <Table striped bordered hover>
+            <Table striped bordered hover responsive>
                 <thead>
                     <tr>
                         <th>#</th>
@@ -30,8 +53,8 @@ export default function Listmenu() {
                         return (
                             <tr>
                                 <td>{val.num}</td>
-                                <td>{val.name}</td>
-                                <td>{val.price}&nbsp;&nbsp; ₩</td>
+                                <td>{val.Name}</td>
+                                <td>{val.Price}&nbsp;&nbsp; ₩</td>
                                 <td><Addanddel /></td>
                             </tr>
                         )
